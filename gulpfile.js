07cce90e75e6f5c1d03ea4a941f4d61cbedcf795
washryan <washryan.3103@gmail.com>
2025-07-18 20:26:48 -0300

    Fix: Configurando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,8 +14,9 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles);
+exports.default = gulp.parallel(styles, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.jsb', gulp.parallel(scripts))
 }
